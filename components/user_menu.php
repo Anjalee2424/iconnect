93c76e83b3ebbb65fec1f9ba5eb11118da15f9bf
yse-yoo <youhei_yoshikawa@yse-c.net>
2025-11-21 12:13:41 +0900

    add user menu

--- components/user_menu.php
+++ components/user_menu.php
@@ -1,7 +1,7 @@
   <!-- Bottom Navigation -->
   <div class="bottom-nav">
-    <div class="nav-item active"><a href="./">💬<br> Connect</a></div>
-    <div class="nav-item active"><a href="./search.php">🔍<br> Search</a></div>
-    <div class="nav-item"><a href="./edit_profile.php">👤<br> Profile</div>
-    <div class="nav-item"><br><a href="./logout.php">Logout</a></div>
+    <div class="nav-item active"><a href="<?= $BASE_URL ?>mypage/">💬<br> Connect</a></div>
+    <div class="nav-item active"><a href="<?= $BASE_URL ?>mypage/search.php">🔍<br> Search</a></div>
+    <div class="nav-item"><a href="<?= $BASE_URL ?>mypage/edit_profile.php">👤<br> Profile</div>
+    <div class="nav-item"><br><a href="<?= $BASE_URL ?>mypage/logout.php">Logout</a></div>
   </div>
